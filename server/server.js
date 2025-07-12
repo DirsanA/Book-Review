fc3de13a8d951e1831f338af65df41bde7d80eb7
DirsanA <dirsanantehun739@gmail.com>
2025-07-12 01:04:24 -0700

    server is setuped by uisng http and express for separtion of concerns

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const PORT = 3000;
 // Create the server and store it in a variable
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!");
+  res.end("Hello from the HTTP server!"); // Add this line
 });
 
 // Call .listen on the server instance
 server.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
   console.log("Press Ctrl+C to stop the server.");
+  console.log("You can access it at http://localhost:" + PORT);
 });
